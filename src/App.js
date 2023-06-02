baafc6ae02099b2bd76ba5b9896383433f6c41a9
Prakash L <121917226+prakashlohanathan@users.noreply.github.com>
2023-06-02 14:07:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Sidebar from "./component/Sidebar";
+import Dashboard from "./component/Dashboard";
+function App() {
+  return <>
+    <div id="wrapper">
+        <Sidebar/>
+        <Dashboard/>
+    </div>
+  </>
+}
+
+export default App;
